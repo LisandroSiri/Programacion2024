27af3cb9ca203b7c5ec25cfdb52229624f310bb5
Lisandro <Lisandro@DESKTOP-616VKCS>
2024-04-14 19:00:50 -0300

    first commit

--- src/TP1/Ejercicio1.java
+++ src/TP1/Ejercicio1.java
@@ -1,22 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package TP1;
 
+import java.util.Scanner;
 /**
  *
  * @author Lisandro
  */
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
+        Scanner notas = new Scanner(System.in);
+        
+        System.out.println("Ingrese la nota de Programacion II:");
+        int x = notas.nextInt();//la nota solo puede ser cargada como un entero.
+       if(x==10){
+           System.out.println("El alumno es sobresaliente en la materia, su nota es:"+x);
+       }
+       else if (x>=4) {
+        System.out.println("El alumno esta aprobado en la materia, su nota es:"+x);
+       }      
+       else if(x<4){   
+        System.out.println("El alumnno esta desaprobbado en la materia;su nota es:"+x);
+       }    
+       
+   }
+    
+
     
     
-    
-}
+
